393b3ce6bd4e24ed2556693e1878b5cf8b7eac75
liuchunfeng <liuchunfeng@jd.com>
2018-12-18 15:12:50 +0800

    update

--- template/unifiedOrder/stepCommon/step5.vue
+++ template/unifiedOrder/stepCommon/step5.vue
@@ -4,43 +4,41 @@
     </div>
 </template>
 <script>
-import mixin from './mixin';
 import { mapGetters } from 'vuex';
+import mixin from './mixin';
 
 // start 此处信息自动注入<% typeList.forEach(function(type){ %>
 import <%= type %>Step5 from '../typeList/<%= type %>/<%= type %>Step5';<% }); %>
 // end 此处信息自动注入
 
 export default {
-    name: 'step5',
-    mixins: [mixin],
-    components: {
-      // start 此处信息自动注入<% typeList.forEach(function(type){%>
-      <%= type %>Step5,<% })%>
-      // end 此处信息自动注入
+  name: 'step5',
+  mixins: [mixin],
+  components: {
+    // start 此处信息自动注入<% typeList.forEach(function(type){%>
+    <%= type %>Step5,<% })%>
+    // end 此处信息自动注入
+  },
+  props: {
+    step: Number,
+  },
+  data() {
+    return {
+      currentView: '',
+    };
+  },
+  computed: {
+    ...mapGetters(['basic']),
+  },
+  mounted() {
+    this.getCurrentView();
+  },
+  methods: {
+    getCurrentView() {
+      const viewName = `${this.lowHorizontalLineToHump(this.basic.orderType)}Step5`;
+      this.currentView = viewName;
     },
-    props: {
-        step: Number,
-    },
-    data() {
-        return {
-            currentView: '',
-        };
-    },
-    computed: {
-        ...mapGetters(['basic']),
-    },
-    mounted() {
-        this.getCurrentView();
-    },
-    methods: {
-        getCurrentView() {
-            let viewName = `${this.lowHorizontalLineToHump(
-                this.basic.orderType,
-            )}Step5`;
-            this.currentView = viewName;
-        },
-    },
+  },
 };
 </script>
 
